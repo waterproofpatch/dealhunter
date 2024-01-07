db5ae15803c18ac98610a215920ea870b2ba81de
waterproofpatch <waterproofpatch@gmail.com>
2024-01-07 00:11:24 -0500

    sort deals by distance

--- src/app/deals.service.ts
+++ src/app/deals.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, map } from 'rxjs';
 
 import { Deal } from './models/deal';
+import { LocationService } from './location.service';
 
 @Injectable({
   providedIn: 'root'
@@ -11,15 +12,17 @@ export class DealsService {
   private deals$: BehaviorSubject<Deal[]> = new BehaviorSubject<Deal[]>([])
   private deals: Deal[] = []
 
-  constructor() { }
+  constructor(private locationService: LocationService) { }
 
   public addDeal(deal: Deal) {
     this.deals.push(deal)
     this.deals$.next(this.deals)
   }
 
-  public getDeals(): BehaviorSubject<Deal[]> {
-
-    return this.deals$
+  public getDeals() {
+    return this.deals$.pipe(
+      map(deals => deals.sort((a, b) => this.locationService.calculateDistance(a.location) - this.locationService.calculateDistance(b.location)))
+    );
   }
+
 }
