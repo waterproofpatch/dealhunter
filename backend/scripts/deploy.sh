44a141f783ae7decebe143a218d02fa4ea069135
waterproofpatch <waterproofpatch@gmail.com>
2024-01-08 00:58:21 -0500

    rev

--- backend/scripts/deploy.sh
+++ backend/scripts/deploy.sh
@@ -1,3 +1,5 @@
 # Run from the dir containing main.go
 GOOS=linux GOARCH=386 go build .
-scp deals waterproofpatch@ssh-waterproofpatch.alwaysdata.net:~/
+scp deals waterproofpatch@ssh-waterproofpatch.alwaysdata.net:~/deals
+#curl -X POST --basic --user "APIKEY: $API_KEY" https://api.alwaysdata.com/v1/site/waterproofpatch/restart/
+curl -X POST --basic --user "$API_KEY:" https://api.alwaysdata.com/v1/site/waterproofpatch.alwaysdata.net/restart/
