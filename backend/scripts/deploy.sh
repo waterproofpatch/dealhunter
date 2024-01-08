9bb8d4c1dff982799de2834532fd1767bf17ca67
waterproofpatch <waterproofpatch@gmail.com>
2024-01-07 19:14:57 -0500

    build and deploy binary

--- backend/scripts/deploy.sh
+++ backend/scripts/deploy.sh
@@ -1,2 +1,3 @@
 # Run from the dir containing main.go
-scp -r ./* waterproofpatch@ssh-waterproofpatch.alwaysdata.net:~/
+GOOS=linux GOARCH=386 go build .
+scp deals waterproofpatch@ssh-waterproofpatch.alwaysdata.net:~/
