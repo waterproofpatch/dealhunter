7ff99ba791d41473ac14c75f3e80c359b66df900
waterproofpatch <waterproofpatch@gmail.com>
2024-01-09 00:34:05 -0500

    forms

--- src/app/authentication/authentication.component.ts
+++ src/app/authentication/authentication.component.ts
@@ -1,18 +1,42 @@
 // authentication.component.ts
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-authentication',
   templateUrl: './authentication.component.html',
   styleUrls: ['./authentication.component.css']
 })
-export class AuthenticationComponent {
-  public mode: string = "";
+export class AuthenticationComponent implements OnInit {
+  mode: string = "";
+  signInForm = new FormGroup({
+    'email': new FormControl(null, [Validators.required, Validators.email]),
+    'password': new FormControl(null, Validators.required)
+  });
+
+  signUpForm = new FormGroup({
+    'email': new FormControl(null, [Validators.required, Validators.email]),
+    'password': new FormControl(null, Validators.required),
+    'confirmPassword': new FormControl(null, Validators.required)
+  });
 
   constructor(private route: ActivatedRoute) {
     this.route.queryParams.subscribe(params => {
       this.mode = params['mode'];
     });
+  }
+
+  ngOnInit() {
+  }
+
+  onSubmit() {
+    if (this.mode === 'signin' && this.signInForm.valid) {
+      // Handle sign in
+      console.log('Sign in:', this.signInForm.value);
+    } else if (this.mode === 'signup' && this.signUpForm.valid) {
+      // Handle sign up
+      console.log('Sign up:', this.signUpForm.value);
+    }
   }
 }
