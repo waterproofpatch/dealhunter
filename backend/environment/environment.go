9e472cc747870cd4d8fb5d45d354941bfcdf4fbb
waterproofpatch <waterproofpatch@gmail.com>
2024-01-09 22:26:29 -0500

    refresh token

--- backend/environment/environment.go
+++ backend/environment/environment.go
@@ -8,14 +8,16 @@ import (
 )
 
 type Environment struct {
-	JWT_SIGNING_KEY             string
-	JWT_ACCESS_TOKEN_EXPIRE_MIN string
-	DB_NAME                     string
-	DB_PASSWORD                 string
-	DB_HOST                     string
-	DB_USER                     string
-	DB_PORT                     string
-	PORT                        string
+	JWT_SIGNING_KEY              string
+	JWT_SIGNING_KEY_REFRESH      string
+	JWT_ACCESS_TOKEN_EXPIRE_MIN  string
+	JWT_REFRESH_TOKEN_EXPIRE_MIN string
+	DB_NAME                      string
+	DB_PASSWORD                  string
+	DB_HOST                      string
+	DB_USER                      string
+	DB_PORT                      string
+	PORT                         string
 }
 
 var gEnv *Environment
@@ -27,14 +29,16 @@ func GetEnvironment() *Environment {
 func Init() error {
 	var env Environment
 	envVars := map[string]*string{
-		"JWT_SIGNING_KEY":             &env.JWT_SIGNING_KEY,
-		"JWT_ACCESS_TOKEN_EXPIRE_MIN": &env.JWT_ACCESS_TOKEN_EXPIRE_MIN,
-		"DB_NAME":                     &env.DB_NAME,
-		"DB_PASSWORD":                 &env.DB_PASSWORD,
-		"DB_HOST":                     &env.DB_HOST,
-		"DB_USER":                     &env.DB_USER,
-		"DB_PORT":                     &env.DB_PORT,
-		"PORT":                        &env.PORT,
+		"JWT_SIGNING_KEY":              &env.JWT_SIGNING_KEY,
+		"JWT_SIGNING_KEY_REFRESH":      &env.JWT_SIGNING_KEY_REFRESH,
+		"JWT_ACCESS_TOKEN_EXPIRE_MIN":  &env.JWT_ACCESS_TOKEN_EXPIRE_MIN,
+		"JWT_REFRESH_TOKEN_EXPIRE_MIN": &env.JWT_REFRESH_TOKEN_EXPIRE_MIN,
+		"DB_NAME":                      &env.DB_NAME,
+		"DB_PASSWORD":                  &env.DB_PASSWORD,
+		"DB_HOST":                      &env.DB_HOST,
+		"DB_USER":                      &env.DB_USER,
+		"DB_PORT":                      &env.DB_PORT,
+		"PORT":                         &env.PORT,
 	}
 
 	for name, ptr := range envVars {
--- backend/tokens/tokens.go
+++ backend/tokens/tokens.go
@@ -33,3 +33,26 @@ func GenerateAccessToken(user models.User) string {
 	}
 	return accessToken
 }
+
+func GenerateRefrehToken(user models.User) string {
+	// Create a new token object, specifying signing method and the claims
+	expireMin, err := strconv.Atoi(environment.GetEnvironment().JWT_REFRESH_TOKEN_EXPIRE_MIN)
+	if err != nil {
+		logging.GetLogger().Error().Msg(err.Error())
+		panic("Failed generating token (1)!")
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"id":    user.ID,
+		"email": user.Email,
+		"exp":   time.Now().Add(time.Minute * time.Duration(expireMin)).Unix(),
+	})
+
+	// Sign and get the complete encoded token as a string using the secret
+	refreshTokenSigningKey := environment.GetEnvironment().JWT_SIGNING_KEY_REFRESH
+	refreshToken, err := token.SignedString([]byte(refreshTokenSigningKey))
+	if err != nil {
+		logging.GetLogger().Error().Msg(err.Error())
+		panic("Failed generating token (2)!")
+	}
+	return refreshToken
+}
