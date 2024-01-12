ab49c897f1db55658a5fc6bc7e9c584594acb19b
waterproofpatch <waterproofpatch@gmail.com>
2024-01-11 22:58:55 -0500

    cookie

--- backend/cookies/cookies.go
+++ backend/cookies/cookies.go
@@ -1,15 +1,19 @@
 package cookies
 
-import "net/http"
+import (
+	"net/http"
+	"time"
+)
 
 func SetRefreshTokenCookie(refreshToken string) *http.Cookie {
 	// Create a cookie
 	cookie := &http.Cookie{
 		Name:     "refresh_token",
 		Value:    refreshToken,
+		Expires:  time.Now().Add(24 * time.Hour), // The cookie will expire in 24 hours
 		HttpOnly: true,
 		Secure:   true,
-		SameSite: http.SameSiteStrictMode,
+		SameSite: http.SameSiteNoneMode,
 	}
 	return cookie
 }
