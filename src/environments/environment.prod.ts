bb66c6103222fc8fa855ce5f03f7843e48ec0169
waterproofpatch <waterproofpatch@gmail.com>
2024-01-17 00:36:22 -0500

    fix url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@
 export const environment = {
 	production: true,
 	// apiUrl: 'https://dealhunter.us-east-2.elasticbeanstalk.com'
-	apiUrl: 'https://dealhunter.alwaysdata.com'
+	apiUrl: 'https://dealhunter.alwaysdata.net'
 };
