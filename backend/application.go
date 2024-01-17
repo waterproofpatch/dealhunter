23df2ef0b8125b73d049c84d685546f8aa0c237b
waterproofpatch <waterproofpatch@gmail.com>
2024-01-16 23:50:36 -0500

    app

--- backend/application.go
+++ backend/application.go
@@ -43,6 +43,6 @@ func main() {
 
 	serverAddress := ":" + port
 	log.Printf("Server starting on %s\n", serverAddress)
-	// log.Fatal(http.ListenAndServe(serverAddress, *handler))
-	log.Fatal(http.ListenAndServeTLS(serverAddress, "server.crt", "server.key", *handler))
+	log.Fatal(http.ListenAndServe(serverAddress, *handler))
+	// log.Fatal(http.ListenAndServeTLS(serverAddress, "server.crt", "server.key", *handler))
 }
