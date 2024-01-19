2812aa190049b13b13c8b7eda14e014f0a7693ea
waterproofpatch <waterproofpatch@gmail.com>
2024-01-18 22:11:27 -0500

    some deploy stuff

--- backend/scripts/deploy.sh
+++ backend/scripts/deploy.sh
@@ -2,9 +2,10 @@
 GOOS=linux GOARCH=386 go build .
 
 # io error when overwriting in-use file, need smarter staging system
+scp scripts/shutdown.sh dealhunter@ssh-dealhunter.alwaysdata.net:~/shutdown.sh
+echo "Running shutdown..."
+ssh dealhunter@ssh-dealhunter.alwaysdata.net 'bash ~/shutdown.sh'
+echo "Ran shutdown..."
 scp deals dealhunter@ssh-dealhunter.alwaysdata.net:~/deals-2
-scp server.crt dealhunter@ssh-dealhunter.alwaysdata.net:~/server.crt
-scp server.key dealhunter@ssh-dealhunter.alwaysdata.net:~/server.key
 
-#curl -X POST --basic --user "APIKEY: $API_KEY" https://api.alwaysdata.com/v1/site/waterproofpatch/restart/
-#curl -X POST --basic --user "$API_KEY:" https://api.alwaysdata.com/v1/site/waterproofpatch.alwaysdata.net/restart/
+curl -X POST --basic --user "$APIKEY account=dealhunter:" https://api.alwaysdata.com/v1/site/dealhunter/restart/
--- backend/scripts/shutdown.sh
+++ backend/scripts/shutdown.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [ -z "$LOCAL_DEV" ]
 then
-  curl -X POST -H "Authorization: Bearer $(echo $SHUTDOWN_ACCESS_TOKEN)" http://localhost:$PORT/shutdown
+  curl -X POST -H "Authorization: Bearer $(echo $SHUTDOWN_ACCESS_TOKEN)" http://dealhunter.alwaysdata.net/shutdown
 else
   curl -k -X POST -H "Authorization: Bearer $(echo $SHUTDOWN_ACCESS_TOKEN)" https://localhost:$PORT/shutdown
 fi
