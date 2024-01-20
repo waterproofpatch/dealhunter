4cc39d42efcde56acdd938bdd8cfa83a5dd325c5
waterproofpatch <waterproofpatch@gmail.com>
2024-01-19 23:52:29 -0500

    store options

--- src/app/deals/deals.component.ts
+++ src/app/deals/deals.component.ts
@@ -11,11 +11,24 @@ import { LocationService } from '../location.service';
 export class DealsComponent implements OnInit {
   public distanceToDeal: number = 5
   public sortOption: string = ""
-  constructor(public dealsService: DealsService,
+  constructor(
+    public dealsService: DealsService,
     public locationService: LocationService,
   ) {
-
   }
+
   ngOnInit(): void {
+    this.sortOption = localStorage.getItem('sortOption') || ""
+    this.distanceToDeal = +(localStorage.getItem('distance') || 5)
+  }
+
+  public saveSortOption(event: any): void {
+    console.log(`Saving sortOption: ${event.value}`)
+    localStorage.setItem('sortOption', event.value)
+  }
+
+  public saveDistanceOption(event: any): void {
+    console.log(`Saving distance: ${event}`)
+    localStorage.setItem('distance', event)
   }
 }
