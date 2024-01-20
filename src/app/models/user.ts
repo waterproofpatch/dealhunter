705063266d766bfacf691cc41ea836b1014234b4
waterproofpatch <waterproofpatch@gmail.com>
2024-01-20 01:20:10 -0500

    moves

--- src/app/models/user.ts
+++
@@ -1,4 +0,0 @@
-export interface User {
-	ID: number,
-	Reputation: number,
-}
--- src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-
-export const environment = {
-	production: true,
-	// apiUrl: 'https://dealhunter.us-east-2.elasticbeanstalk.com'
-	apiUrl: 'https://dealhunter.alwaysdata.net'
-};
